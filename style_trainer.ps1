33b69c4359b9fbbbaa7c75c215b301e7d5bf3341
Heather M. <heatherkmorrison@gmail.com>
2025-12-31 14:20:19 -0800

    CLI: Powershell Version
    This is a user friendly CLI tool for fine tuning pythia-2.8b using LoRA. At this stage it is taking user input and printing the user's choices but needs to be updated to call style_trainer.py

---
+++ style_trainer.ps1
@@ -0,0 +1,64 @@
+$corpora = Get-ChildItem -Filter *.json
+$valid = $false
+while (-not $valid){
+	Write-Host "Which corpus would you like to fine-tune with?"
+
+	for ($i = 0; $i -lt $corpora.Count; $i++){
+		Write-Host "${i}: $($corpora[$i].Name)"
+	}
+	$corpus = [int](Read-Host "Please enter a number")
+	if ($corpus -ge 0 -and $corpus -lt $corpora.Count){
+		Write-Host "You selected $($corpora[$corpus].Name)"
+		$yesno = $false
+		while (-not $yesno){
+			$correct = Read-Host "Is this correct? (y)es or (n)o"
+			if ($correct.StartsWith("Y", [System.StringComparison]::OrdinalIgnoreCase)){
+				$valid = $true
+				$yesno = $true
+			} elseif ($correct.StartsWith("N", [System.StringComparison]::OrdinalIgnoreCase)){
+				$yesno = $true
+			} else {
+				Write-Host "Invalid Selction"
+				Start-Sleep -Seconds 1
+			}
+		}
+	}
+	else {
+		Write-Host "Invalid Selction"
+		Start-Sleep -Seconds 1
+	}
+}
+
+$valid = $false
+
+while(-not $valid){
+	$new = Read-Host "Is this a new adapter? (y)es or (n)o"
+	$fresh = $true
+	if ($correct.StartsWith("Y", [System.StringComparison]::OrdinalIgnoreCase)){
+				$valid = $true
+				
+			} elseif ($correct.StartsWith("N", [System.StringComparison]::OrdinalIgnoreCase)){
+				$fresh = $false
+				$valid = $true
+			} else {
+				Write-Host "Invalid Selction"
+				Start-Sleep -Seconds 1
+			}
+}
+
+$valid = $false
+
+while(-not $valid){
+	$steps = (Read-Host "How many total steps would you like to train? Please enter a whole number")
+	try{
+		$total = [int]$steps
+		$valid = $true
+		
+	} catch {
+		Write-Host "Invalid Selction"
+		Start-Sleep -Seconds 1
+	}
+}
+Write-Host $corpora[$corpus].Name
+Write-Host $fresh
+Write-Host $total
