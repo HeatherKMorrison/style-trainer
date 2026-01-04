9fc54326af5f0bec5b8a5f6ba3c98a1df5ce6967
Heather M. <heatherkmorrison@gmail.com>
2026-01-03 19:17:36 -0800

    Bash CLI
    - needs to check for possible numbers attached to corpora

---
+++ style_trainer.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+corpora=( *.json )
+valid=false
+
+while [[ $valid == false ]]; do
+	yesno=false
+	
+	echo "Which corpus would you like to fine-tune with?"
+	i=0
+	for corpus in "${corpora[@]}"; do
+		echo "$i: $corpus"
+		((i++))
+	done
+	read -p "Please enter a number:" number
+	echo "You selected ${corpora[$number]}."
+	while [[ $yesno == false ]]; do
+		read -p "Is this correct? (y)es or (n)o: " confirm
+		shopt -s nocasematch
+		if [[ $confirm == y* ]]; then
+			yesno=true
+			valid=true
+		elif [[ $confirm == n* ]]; then
+			yesno=true
+		else
+			echo "Invalid input"
+		fi
+	done
+	
+done
+
+valid=false
+
+while [[ $valid == false ]]; do
+	read -p "Is this a new adapter? (y)es or (n)o: " new
+	fresh=true
+	shopt -s nocasematch
+	if [[ new == y* ]]; then
+		valid=true
+				
+	elif [[ new == n* ]]; then
+		fresh=false
+		valid=true
+	else
+		echo "Invalid Selction"
+				
+	fi
+done
+
+valid=false
+
+while [[ $valid == false ]]; do
+	read -p "How many total steps would you like to train? Please enter a whole number: " steps
+	if [[ $steps =~ ^-?[0-9]+$ ]]; then
+		valid=true
+		
+	else
+		echo "Invalid Selction"
+	
+	fi
+done
+
+python style_trainer.py $fresh ${corpora[$number]} $steps
